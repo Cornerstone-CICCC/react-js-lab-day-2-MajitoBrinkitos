b24141ee8f2dcd4795fead90f84cd0b9c7f64272
Majito Brinkitos <mcvef2013@gmail.com>
2025-05-12 08:33:13 -0700

    lab day

--- react-lab-2/src/components/Task.tsx
+++ react-lab-2/src/components/Task.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
+
 function Task() {
+    const [task, setTask] = useState([]);
+    const [taskText, setTaskText] = useState('');
+
+    {/*Add Task Functionality */}
     return (
         <div className="grid grid-cols-3 gap-4 col-start-2 bg-orange-100 text-black p-4 rounded-md">
-            <h1 className="col-span-3 col-start-1 flex justify-center">Tasks</h1>
-            <label htmlFor="task">
-                <input name="text" defaultValue="Enter a new text" />
-            </label>
+            <h1 className="col-span-3 flex justify-center">Tasks</h1>
+
+            <div className="col-span-3 flex items-center space-x-4 border border-black p-4 rounded-md">
+                <label htmlFor="task" className="w-3/4">
+                    <input className="w-full p-2 border border-gray-400 rounded-md" name="text" defaultValue="Enter a new task" />
+                </label>
+                <button type="button" className="px-4 py-2 border border-black rounded-md">
+                    Add Task
+                </button>
+            </div>
         </div>
     );
 }
 
-export default Task
+export default Task;
